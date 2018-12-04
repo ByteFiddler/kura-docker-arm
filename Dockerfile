6617a59395daf71b2c2083b907a5496c2e3c40da
Wuodan <stefan.kuhn.zurich@gmail.com>
2018-12-04 20:54:28 +0100

    Use resin/rpi-raspbian to easily support arm32v6

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,4 @@
-ARG ARCH=arm32v7
-FROM $ARCH/openjdk:8-jre
+FROM resin/rpi-raspbian:stretch
 
 ARG KURA_VERSION=4.0.0
 
@@ -7,11 +6,13 @@ RUN apt-get update && \
 	apt-get --yes install	apt-utils && \
 	apt-get --yes install	bluetooth \
 							gdebi-core \
+							openjdk-8-jre-headless \
 							procps \
 							usbutils && \
 	wget -q http://download.eclipse.org/kura/releases/${KURA_VERSION}/kura_${KURA_VERSION}_raspberry-pi-2-3-nn_installer.deb && \
 	yes | gdebi kura_${KURA_VERSION}_raspberry-pi-2-3-nn_installer.deb && \
 	rm kura_${KURA_VERSION}_raspberry-pi-2-3-nn_installer.deb && \
+	apt-get remove gdebi && \
 	apt-get clean
 
 COPY start.sh /
--- build.sh
+++ build.sh
@@ -2,23 +2,17 @@
 
 set -e
 
-if [ -z "$1" ]; then
-	echo "No ARCH specified. Example: ./build.sh arm32v7" 1>&2
-	exit 1
-fi
-
-ARCH="$1"
-
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-REPO=wuodan
-IMAGE=$(basename $DIR)
-TAG="$(docker info -f '{{.Architecture}}')"
-FULL_TAG=${REPO}/${IMAGE}:${TAG}
 DATETIME=$(date '+%Y%m%d%H%M%S')
+
+REPO=bytefiddler
+IMAGE=$(basename $DIR)
+TAG="latest"
+FULL_TAG=${REPO}/${IMAGE}:${TAG}
 
 mkdir -p ${DIR}/log
 echo "Build image, write log to : ${DIR}/log/docker-build.${DATETIME}.log"
-docker build --build-arg ARCH="${ARCH}" --tag ${FULL_TAG} $DIR 2>&1 | tee ${DIR}/log/docker-build.${DATETIME}.log
+docker build --tag ${FULL_TAG} $DIR 2>&1 | tee ${DIR}/log/docker-build.${DATETIME}.log
 
 echo "Push image ..."
 docker push ${FULL_TAG}
