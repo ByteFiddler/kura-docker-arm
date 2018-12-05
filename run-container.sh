fcd3d0530ac8153204695cf71bff181bfc484511
Wuodan <stefan.kuhn.zurich@gmail.com>
2018-12-05 17:43:00 +0100

    Improve run script

--- run-container.sh
+++ run-container.sh
@@ -9,4 +9,10 @@ IMAGE=$(basename $DIR)
 TAG="latest"
 FULL_TAG=${REPO}/${IMAGE}:${TAG}
 
-docker run -d -p 80:80 $FULL_TAG
+GLUSTER_MOUNT=/mnt/kura
+
+if [ -d $GLUSTER_MOUNT ]; then
+	docker run -d -p 80:80 -v $GLUSTER_MOUNT:$GLUSTER_MOUNT $FULL_TAG
+else
+	docker run -d -p 80:80 $FULL_TAG
+fi
