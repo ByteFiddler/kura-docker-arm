b06aa04fdb2e5b7bf2cad25ac40ba0a793321338
Wuodan <stefan.kuhn.zurich@gmail.com>
2018-12-05 19:23:07 +0100

    Create docker manifest

--- build.sh
+++ build.sh
@@ -7,12 +7,18 @@ DATETIME=$(date '+%Y%m%d%H%M%S')
 
 REPO=bytefiddler
 IMAGE=$(basename $DIR)
-TAG="latest"
+TAG="arm"
 FULL_TAG=${REPO}/${IMAGE}:${TAG}
 
 mkdir -p ${DIR}/log
 echo "Build image, write log to : ${DIR}/log/docker-build.${DATETIME}.log"
 docker build --tag ${FULL_TAG} $DIR 2>&1 | tee ${DIR}/log/docker-build.${DATETIME}.log
+
+TAG="latest"
+FULL_TAG=${REPO}/${IMAGE}:${TAG}
+
+docker manifest create ${FULL_TAG} ${FULL_TAG} | exit 0
+docker manifest annotate --arch arm ${FULL_TAG} ${FULL_TAG}
 
 echo "Push image ..."
 docker push ${FULL_TAG}
